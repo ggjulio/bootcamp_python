73b1deeac8f6bafc6f0164662dfa8a80179b6f32
ggjulio <juligonz@student.42.fr>
2020-01-13 14:00:54 +0100

    ex02 done

--- ex02/whois.py
+++ ex02/whois.py
@@ -1,8 +1,6 @@
 import sys
 
-if len(sys.argv) > 1:
-
-  lst = sys.argv[1:]
-  str = "".join(lst)[::-1]
-  str = str.swapcase()
-  print (str)
+if len(sys.argv) == 2:
+    print ("EVEN") if int(sys.argv[1]) % 2 == 0 else print ("ODD")
+else:
+    print ("ERROR")
