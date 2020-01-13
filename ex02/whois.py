55acd979caca02e0f7f945632566734d83c3d82b
ggjulio <juligonz@student.42.fr>
2020-01-13 14:17:49 +0100

    ex02 done

--- ex02/whois.py
+++ ex02/whois.py
@@ -1,6 +1,10 @@
 import sys
 
 if len(sys.argv) == 2:
-    print ("EVEN") if int(sys.argv[1]) % 2 == 0 else print ("ODD")
+    n = int(sys.argv[1])
+    if n != 0:
+    	print ("I'm Even.") if n % 2 == 0 else print ("I'm odd.")
+    else:
+    	print ("I'm Zero.")
 else:
     print ("ERROR")
