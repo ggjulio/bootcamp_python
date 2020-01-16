cab4fea0a4e63f4d1ccc5bb41e28785dd673c3e9
ggjulio <juligonz@student.42.fr>
2020-01-16 22:13:04 +0100

    ex02 done

--- day01/ex02/vector.py
+++ day01/ex02/vector.py
@@ -1,28 +1,31 @@
 class Vector:
-    def __init__(self, vector: list):
+    def __init__(self, vector):
         self.vector = vector
         self.size = len(self.vector)
 
     def __add__(self, x):
-        self.vector = [v + x for v in self.vector]
+        return(Vector([v + x for v in self.vector]))
 
-#    def __radd__(self, x):
-#        self.vector = [v + x for v in self.vector]
+    def __radd__(self, x):
+        return(Vector([v + x for v in self.vector]))
 
     def __sub__(self, x):
-        self.vector = [v - x for v in self.vector]
+        return(Vector([v - x for v in self.vector]))
 
     def __rsub__(self, x):
-        self.vector = [v - x for v in self.vector]
+        return(Vector([v - x for v in self.vector]))
 
     def __truediv__(self, x):
-        self.vector = [v / x for v in self.vector]
+        return(Vector([v / x for v in self.vector]))
 
     def __rtruediv__(self, x):
-        self.vector = [v / x for v in self.vector]
+        return(Vector([v / x for v in self.vector]))
 
     def __mul__(self, x):
-        self.vector = []
+        return(Vector([v * x for v in self.vector]))
+
+    def __rmul__(self, x):
+        return(Vector([v * x for v in self.vector]))
 
     def __repr__(self):
         return (self.vector.__repr__())
