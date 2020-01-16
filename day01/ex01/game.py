f557cbf6ff8b0e09d7d1e9c8766eb94ee98c7222
ggjulio <juligonz@student.42.fr>
2020-01-16 18:54:41 +0100

    day01 - ex01 done

--- day01/ex01/game.py
+++ day01/ex01/game.py
@@ -1,12 +1,12 @@
 
+
 class GotCharacter:
-    def __init__(self, first_name: str, is_alive=True):
+    def __init__(self, first_name, is_alive=True):
         self.first_name = first_name
         self.is_alive = is_alive
 
-            
-
 class Stark(GotCharacter):
+    """A class representing the Stark family. Or when bad things happen to good people."""
     def __init__(self, first_name=None, is_alive=True):
         super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Stark"
@@ -16,4 +16,9 @@ class Stark(GotCharacter):
         print (self.house_word)
 
     def die(self):
-        GotCharacter.is_alive = False
+        self.is_alive = False
+
+    def __repr__(self):
+        return (
+        f"Stark({self.first_name}, {self.family_name}, {self.house_word},{self.is_alive})"
+        )
